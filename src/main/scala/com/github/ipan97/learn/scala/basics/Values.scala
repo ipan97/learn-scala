ed0726dfe3f5dd57c1dd02c5f66764193230e596
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-01-16 12:04:42 +0700

    Values

---
+++ src/main/scala/com/github/ipan97/learn/scala/basics/Values.scala
@@ -0,0 +1,12 @@
+package com.github.ipan97.learn.scala.basics
+
+/**
+  * @author Ipan Taufik Rahman
+  */
+object Values extends App {
+  // Values
+  val x = 1 + 1
+  println(x)
+  val y: Int = 1 + 2
+  println(y)
+}
