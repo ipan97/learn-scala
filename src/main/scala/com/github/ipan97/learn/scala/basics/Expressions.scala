1005836c99d1701b570dc23f49d7fb362d881b3b
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-01-16 12:04:10 +0700

    Expressions

---
+++ src/main/scala/com/github/ipan97/learn/scala/basics/Expressions.scala
@@ -0,0 +1,12 @@
+package com.github.ipan97.learn.scala.basics
+
+/**
+  * @author Ipan Taufik Rahman
+  */
+object Expressions extends App {
+  // Expressions
+  println(1)
+  println(1 + 1)
+  println("Hello!")
+  println("Hello," + "word!")
+}
