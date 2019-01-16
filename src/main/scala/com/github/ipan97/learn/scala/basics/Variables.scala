53400da14a45801658a8010aa0abf7d74258a077
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-01-16 12:13:00 +0700

    Learn variable

---
+++ src/main/scala/com/github/ipan97/learn/scala/basics/Variables.scala
@@ -0,0 +1,18 @@
+package com.github.ipan97.learn.scala.basics
+
+/**
+  * @author Ipan Taufik Rahman
+  */
+object Variables extends App {
+  // Variables
+  var x = 1+1
+  x = 3
+  println(x)
+
+  val y = 1+1
+  //y = 3
+  println(y)
+
+  var z: Int = 1+10
+  println(z)
+}
