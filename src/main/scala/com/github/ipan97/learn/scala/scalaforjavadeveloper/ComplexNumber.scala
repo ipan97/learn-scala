2875ff7be626753c55eee2fe07db01920910f0d2
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-01-17 14:41:17 +0700

    scala-for-java-developer class

---
+++ src/main/scala/com/github/ipan97/learn/scala/scalaforjavadeveloper/ComplexNumber.scala
@@ -0,0 +1,11 @@
+package com.github.ipan97.learn.scala.scalaforjavadeveloper
+
+/**
+  * @author Ipan Taufik Rahman
+  */
+object ComplexNumber {
+  def main(args: Array[String]): Unit = {
+    val c = new Complex(1.2, 3.4)
+    println("imaginary part : " + c.im)
+  }
+}
---
+++ src/main/scala/com/github/ipan97/learn/scala/scalaforjavadeveloper/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.github.ipan97.learn.scala.scalaforjavadeveloper
+
+/**
+  * @author Ipan Taufik Rahman
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+}
